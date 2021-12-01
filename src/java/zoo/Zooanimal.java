8c1afac8b7fb5e722df6b0e567ee2bf4e3277225
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-12-01 12:45:43 +0100

    Képesítő pót-pótvizsga

---
+++ src/java/zoo/Zooanimal.java
@@ -0,0 +1,24 @@
+package hu.nive.ujratervezes.kepesitovizsga_pot_potvizsga.zoo;
+
+public class Zooanimal {
+    String Name;
+    int Length;
+    int Weight;
+    String Type;
+    public Zooanimal() {
+
+    }
+    public String getName() {
+        return Name;
+    }
+    public int getLength()
+    {
+        return Length;
+    }
+    public int getWeight() {
+        return Weight;
+    }
+    public String getType() {
+        return Type;
+    }
+}
