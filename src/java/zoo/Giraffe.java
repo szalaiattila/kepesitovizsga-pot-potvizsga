7addc7db1ee37dd51562b6526489926c38042733
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-12-01 12:43:27 +0100

    Képesítő pót-pótvizsga

--- src/java/zoo/Giraffe.java
+++ src/java/zoo/Giraffe.java
@@ -1,25 +1,9 @@
 package hu.nive.ujratervezes.kepesitovizsga_pot_potvizsga.zoo;
 
-public class Giraffe {
-    String Name;
-    int Length;
-    int Weight;
-    String Type;
-    public Giraffe(String Name,int Length){
+public class Giraffe extends Zooanimal{
+    public Giraffe(String Name,int Length) {
         this.Name = Name;
         this.Length = Length;
-    }
-    public String getName() {
-        return Name;
-    }
-    public int getLength()
-    {
-        return Length;
-    }
-    public int getWeight() {
-        return Weight;
-    }
-    public String getType() {
-        return Type;
+
     }
 }
