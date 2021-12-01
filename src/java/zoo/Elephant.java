159424d72a8890411ba678a638846d4d2e61b6c1
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-12-01 12:42:50 +0100

    Képesítő pót-pótvizsga

--- src/java/zoo/Elephant.java
+++ src/java/zoo/Elephant.java
@@ -1,26 +1,9 @@
 package hu.nive.ujratervezes.kepesitovizsga_pot_potvizsga.zoo;
 
-public class Elephant {
-    String Name;
-    int Length;
-    int Weight;
-    String Type;
+public class Elephant extends Zooanimal {
     public Elephant(String Name,int Length,int Weight) {
         this.Name = Name;
         this.Length = Length;
         this.Weight = Weight;
-    }
-    public String getName() {
-        return Name;
-    }
-    public int getLength()
-    {
-        return Length;
-    }
-    public int getWeight() {
-        return Weight;
-    }
-    public String getType() {
-        return Type;
     }
 }
