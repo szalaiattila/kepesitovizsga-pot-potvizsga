ab85cf27e5537a3786561e1fae2c7b83358ad6b2
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-12-01 11:47:40 +0100

    Képesítő pót-pótvizsga

---
+++ src/java/NumberOfDigits/zoo/Lion.java
@@ -0,0 +1,26 @@
+package hu.nive.ujratervezes.kepesitovizsga_pot_potvizsga.zoo;
+
+public class Lion {
+        String Name;
+        int Length;
+        int Weight;
+        String Type;
+        public Lion(String Name) {
+            this.Name = Name;
+        }
+        public String getName() {
+            return Name;
+        }
+        public int getLength()
+        {
+            return Length;
+        }
+        public int getWeight() {
+            return Weight;
+        }
+        public String getType() {
+            return Type;
+        }
+    }
+
+}
